8afe6f67628e52cc6d1d825695159403515f86c4
古书柳 <gushuliu@cqt01.com>
2022-05-20 09:09:41 +0800

    complete annotations

--- gresult/gresult_success.go
+++ gresult/gresult_success.go
@@ -5,6 +5,7 @@
 // You can obtain one at https://github.com/gogf/gf.
 // Project Name :   gf-security
 // Author       :   liushuku@yeah.net
+
 package gresult
 
 import "github.com/gogf/gf/v2/net/ghttp"
@@ -15,7 +16,7 @@ func Success(request *ghttp.Request) {
 	Response(successCode, nil, successMsg, request)
 }
 
-// SucceedData Data in response to successful operation
+// SuccessData Data in response to successful operation
 func SuccessData(data interface{}, request *ghttp.Request) {
 	Response(successCode, data, successMsg, request)
 }
@@ -26,7 +27,7 @@ func SuccessDataTotal(data interface{}, total int64, request *ghttp.Request) {
 	ResponseTotal(successCode, data, successMsg, total, false, request)
 }
 
-// SuccessDataTotal Response success data and total
+// SuccessDataTotalInner Response success data and total
 // This method total will be inside data
 func SuccessDataTotalInner(data interface{}, total int64, request *ghttp.Request) {
 	ResponseTotal(successCode, data, successMsg, total, true, request)
